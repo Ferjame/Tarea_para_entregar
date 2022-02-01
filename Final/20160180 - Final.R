1c3d2cde67a4b82e0b04b43e6ef5113c9f6732a3
Fernando Caldas <fernando.caldas@unmsm.edu.pe>
2022-01-31 23:56:26 -0500

    Add files via upload

---
+++ Final/20160180 - Final.R
@@ -0,0 +1,31 @@
+
+### Pregunta 11
+
+Preg11 <- function(a,b,c){
+  # a : L = Longitud del cauce principal
+  # b : Cota superio
+  # c : Cota inferior
+  cuerpo <- (0.3)*((a)/(((b+c)/2)^(1/4)))^(0.76)
+  print(cuerpo)
+}
+
+Preg11(1200,190,80)
+
+### Pregunta 12
+
+data("airquality")
+
+### Preungta 13
+
+Preg13 <- read.csv("D:/Programas/Python - Tareas/PROGRAMACION - 2021/Final/dataset/p_13_14/gbif_peru.csv")
+
+### Preungta 14
+
+
+
+### Preungta 15
+
+
+#dataset <- read.delim("https://raw.githubusercontent.com/fhernanb/datos/master/Paises.txt")
+
+
